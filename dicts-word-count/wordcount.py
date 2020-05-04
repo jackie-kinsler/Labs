47077daab8c44356deedfc3d78f25b596ea2afc9
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-04 12:23:20 -0700

    Count occurrence of each word in textfile. Need improvement to handle punctuation

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,1 +1,17 @@
-# put your code here.
+
+def make_word_counts(textfile):
+    
+    wordcount_in_textfile = {}
+
+    textfile = open(textfile)
+
+    for line in textfile:
+        words = line.rstrip().split(" ")
+        for word in words:
+            wordcount_in_textfile[word] = wordcount_in_textfile.get(word, 0) +1
+
+    textfile.close
+
+    return wordcount_in_textfile
+
+print(make_word_counts("test.txt"))
