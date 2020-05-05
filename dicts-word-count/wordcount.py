e701390a9466f76e06c1dbf69441d90d7713d761
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-04 17:27:59 -0700

    Now can take any textfile name that is typed into the commmand line. Implemented this functionality with sys.argv

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,7 +1,7 @@
 
-import sys 
+import sys
 
-def make_word_counts(sys.argv[1]):
+def make_word_counts():
     
     wordcount_in_textfile = {}
 
@@ -9,25 +9,22 @@ def make_word_counts(sys.argv[1]):
     #In this case, the other character is None
     #Table handles: .(46) ,(44) !(33) ?(63) ;(59) :(58) _(95) "(34)
     punctuation = { 44 : None, 46 : None, 33 : None, 63 : None, 59 : None, 
-                    58 : None, 95 : None, 34 : None} 
+                    58 : None, 95 : None, 34 : None}
 
-    textfile = open(textfile)
+    textfile = open(sys.argv[1])
 
     for line in textfile:
         words = line.rstrip().split(" ")  #check if .strip() works better!
         for word in words:
             word_without_punctuation = word.translate(punctuation).lower()
-            wordcount_in_textfile[word_without_punctuation] = wordcount_in_textfile.get(word_without_punctuation, 0) +1
-
-    textfile.close
+            wordcount_in_textfile[word_without_punctuation] = wordcount_in_textfile.get(word_without_punctuation, 0) + 1
 
     for word_pair in wordcount_in_textfile.items():
         print(word_pair[0], word_pair[1])
 
-    return
+    textfile.close
 
-make_word_counts("twain.txt")
-
+make_word_counts()
 
 
 
